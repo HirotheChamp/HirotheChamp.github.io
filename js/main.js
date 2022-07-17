0527f75d686a31be329be2eaa5171ca71d3206b8
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2022-07-17 15:35:17 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,11 +3,63 @@ const hamburger = document.querySelector('.menu-btn_burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav_item');
-
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('.menu-nav_item')
+let inputBox = document.querySelectorAll('.inputBox')
+let projectLinks = document.querySelectorAll('.projects_item')
 let showMenu = false;
 
 
+
 menuBtn.addEventListener('click', toggleMenu);
+window.addEventListener('mousemove', cursor);
+
+function cursor(e){
+     mouseCursor.style.top = e.clientY + 'px';
+     mouseCursor.style.left = e.clientX + 'px';
+
+}
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+        mouseCursor.classList.remove('link-grow');
+        // link.classList.remove('menu-nav-item');
+    });
+
+    link.addEventListener('mouseover', () => {
+        mouseCursor.classList.add('link-grow');
+        // link.classList.add('menu-nav-item')
+    });
+
+    projectLinks.forEach(link => {
+        link.addEventListener('mouseleave', () => {
+            mouseCursor.classList.remove('link-shrink');
+           
+        });
+    
+        link.addEventListener('mouseover', () => {
+            mouseCursor.classList.add('link-shrink');
+           
+        });
+ 
+    });
+
+    inputBox.forEach(link => {
+        link.addEventListener('mouseleave', () => {
+            mouseCursor.classList.remove('link-change');
+            // link.classList.remove('menu-nav-item');
+        });
+    
+        link.addEventListener('mouseover', () => {
+            mouseCursor.classList.add('link-change');
+            // link.classList.add('menu-nav-item')
+        });
+
+    });
+    
+});
+
+
 
 function toggleMenu(){
     if(!showMenu){
